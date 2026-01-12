cc1f29ad3e83706cacbbd09f54857946ce4dedc5
kandev <kdev99248@gmail.com>
2026-01-12 12:22:17 +0700

    update fix

--- src/boss/list/XenBoHung.java
+++ src/boss/list/XenBoHung.java
@@ -3,6 +3,7 @@ package boss.list;
 /*
  * @Author: NgojcDev
  */
+
 import consts.ConstPlayer;
 import control.boss.Boss;
 import control.boss.BossesData;
@@ -48,7 +49,7 @@ public class XenBoHung extends Boss {
     }
 
     private void hapThu() {
-        if (!Util.canDoWithTime(this.lastTimeHapThu, this.timeHapThu) || !Util.isTrue(1, 100)) {
+        if (!Util.canDoWithTime(this.lastTimeHapThu, this.timeHapThu) || !Util.isTrue(50, 100)) {
             return;
         }
 
@@ -57,17 +58,19 @@ public class XenBoHung extends Boss {
             return;
         }
         ChangeMapService.gI().changeMapYardrat(this, this.zone, pl.location.x, pl.location.y);
-        this.nPoint.dameg += (pl.nPoint.dame * 5 / 100);
-        this.nPoint.hpg += (pl.nPoint.hp * 2 / 100);
-        this.nPoint.critg++;
-        this.nPoint.calPoint();
-        PlayerService.gI().hoiPhuc(this, pl.nPoint.hp, 0);
-        pl.injured(null, pl.nPoint.hpMax, true, false);
-        Service.gI().sendThongBao(pl, "Bạn vừa bị " + this.name + " hấp thu!");
-        this.chat(2, "Ui cha cha, kinh dị quá. " + pl.name + " vừa bị tên " + this.name + " nuốt chửng kìa!!!");
-        this.chat("Haha, ngọt lắm đấy " + pl.name + "..");
-        this.lastTimeHapThu = System.currentTimeMillis();
-        this.timeHapThu = Util.nextInt(10000, 20000);
+        if (this.nPoint.hpg < 200_000_000) {
+            this.nPoint.dameg += (pl.nPoint.dame * 5 / 100);
+            this.nPoint.hpg += (pl.nPoint.hp * 2 / 100);
+            this.nPoint.critg++;
+            this.nPoint.calPoint();
+            PlayerService.gI().hoiPhuc(this, pl.nPoint.hp, 0);
+            pl.injured(null, pl.nPoint.hpMax, true, false);
+            Service.gI().sendThongBao(pl, "Bạn vừa bị " + this.name + " hấp thu!");
+            this.chat(2, "Ui cha cha, kinh dị quá. " + pl.name + " vừa bị tên " + this.name + " nuốt chửng kìa!!!");
+            this.chat("Haha, ngọt lắm đấy " + pl.name + "..");
+            this.lastTimeHapThu = System.currentTimeMillis();
+            this.timeHapThu = Util.nextInt(10000, 20000);
+        }
     }
 
     @Override
